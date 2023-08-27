f19bbc93b360671f7f235ed181d2dba372a3f1ea
Samuel <xxsephirothxx13@gmail.com>
2023-08-26 21:19:23 -0700

    Exercise Lab 4 help

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ def main():
     # Comlete your code here
     ##################################################
     pass
-
 original_str = ("Python Programming")
-sub1 = original_str[:6]
+sub1 = original_str[0:6]
 sub2 = original_str[7:]
 merged_str = sub2 + sub1
+
 print(sub2)
 print(sub1)
 print(merged_str)
