1456118f8ac29e5f93f229124564f96b72ff6e21
Samuel <xxsephirothxx13@gmail.com>
2023-08-26 21:28:52 -0700

    Exercise Lab 4 is this it?

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ def main():
     ##################################################
     pass
 original_str = ("Python Programming")
-sub1 = original_str[0:6]
+sub1 = original_str[:6]
 sub2 = original_str[7:]
-merged_str = sub2 + sub1
+merged_str = sub2 + " " + sub1
 
 print(sub2)
 print(sub1)
