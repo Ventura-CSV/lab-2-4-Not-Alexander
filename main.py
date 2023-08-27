5505fa42dc5b14733303c381fd545f7ab3fe5122
Samuel <xxsephirothxx13@gmail.com>
2023-08-26 21:09:19 -0700

    Excercise Lab 4

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ def main():
     ##################################################
     pass
 
+original_str = ("Python Programming")
+sub1 = original_str[:6]
+sub2 = original_str[7:]
+merged_str = sub2 + sub1
+print(sub2)
+print(sub1)
+print(merged_str)
 
 if __name__ == '__main__':
     main()
